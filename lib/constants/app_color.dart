8fa42016436c4b82a55eb2ccb9500aecd52a4514
Eznira <ihimarinze@gmail.com>
2025-03-14 14:37:31 +0100

    added app_color

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color green = Color(0xFF32BA78); //jungle green
+  static const Color pink = Color(0xFFFF5A6E); //wild watermelon
+  static const Color grey = Color(0xFFF4F4F6); //athens grey
+  static const Color white = Colors.white;
+}
