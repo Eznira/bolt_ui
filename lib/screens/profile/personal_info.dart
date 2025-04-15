0583bcf579c710162834d703ffde459f5e4529ac
Eznira <ihimarinze@gmail.com>
2025-04-15 13:13:18 +0100

    personal_info screen responsive

--- lib/screens/profile/personal_info.dart
+++ lib/screens/profile/personal_info.dart
@@ -12,167 +12,169 @@ class PersonalInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 12),
-            child: BackButton(),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 12,
-              horizontal: 20,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 12),
+              child: BackButton(),
             ),
-            child: Text(
-              "Personal Info",
-              style: AppTextStyle.title1,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                      ),
-                      child: Icon(
-                        Icons.person_2_outlined,
-                        size: 20,
-                        color: Colors.grey.shade700,
-                      ),
-                    ),
-                    Text(
-                      "Arinze Ihim",
-                      style: AppTextStyle.body1,
-                    ),
-                  ],
-                ),
-                TextButton(
-                  onPressed: () {
-                    // TODO: route to edit
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const UpdateName(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    'Edit',
-                    style: TextStyle(color: Colors.green, fontSize: 16.sp),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              height: 4,
-              child: Divider(
-                height: 4,
-                color: Colors.grey.shade700,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 20,
+              ),
+              child: Text(
+                "Personal Info",
+                style: AppTextStyle.title1,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ),
+                        child: Icon(
+                          Icons.person_2_outlined,
+                          size: 20,
+                          color: Colors.grey.shade700,
+                        ),
                       ),
-                      child: Icon(
-                        Icons.phone_android,
-                        size: 20,
-                        color: Colors.grey.shade700,
+                      Text(
+                        "Arinze Ihim",
+                        style: AppTextStyle.body1,
                       ),
-                    ),
-                    Text(
-                      "+23408078140648",
-                      style: AppTextStyle.body1,
-                    ),
-                  ],
-                ),
-                TextButton(
-                  onPressed: () {
-                    // TODO: route to edit
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const UpdatePhoneNumber(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    'Edit',
-                    style: TextStyle(color: Colors.green, fontSize: 16.sp),
+                    ],
                   ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SizedBox(
-              height: 4,
-              child: Divider(
-                height: 4,
-                color: Colors.grey.shade700,
+                  TextButton(
+                    onPressed: () {
+                      // TODO: route to edit
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const UpdateName(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Edit',
+                      style: TextStyle(color: Colors.green, fontSize: 16.sp),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                      ),
-                      child: Icon(
-                        Icons.email_outlined,
-                        size: 20,
-                        color: Colors.grey.shade700,
-                      ),
-                    ),
-                    Text(
-                      "ihimarinze@gmail.com",
-                      style: AppTextStyle.body1,
-                    ),
-                  ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                height: 4,
+                child: Divider(
+                  height: 4,
+                  color: Colors.grey.shade700,
                 ),
-                TextButton(
-                  onPressed: () {
-                    // TODO: route to edit
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const UpdateEmail(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    'Edit',
-                    style: TextStyle(color: Colors.green, fontSize: 16.sp),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ),
+                        child: Icon(
+                          Icons.phone_android,
+                          size: 20,
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                      Text(
+                        "+23408078140648",
+                        style: AppTextStyle.body1,
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // TODO: route to edit
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const UpdatePhoneNumber(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Edit',
+                      style: TextStyle(color: Colors.green, fontSize: 16.sp),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                height: 4,
+                child: Divider(
+                  height: 4,
+                  color: Colors.grey.shade700,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ),
+                        child: Icon(
+                          Icons.email_outlined,
+                          size: 20,
+                          color: Colors.grey.shade700,
+                        ),
+                      ),
+                      Text(
+                        "ihimarinze@gmail.com",
+                        style: AppTextStyle.body1,
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // TODO: route to edit
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const UpdateEmail(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Edit',
+                      style: TextStyle(color: Colors.green, fontSize: 16.sp),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
