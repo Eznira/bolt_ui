86a13f7101fee8cc09027b6473351f84b4f66c21
Eznira <ihimarinze@gmail.com>
2025-04-15 13:19:05 +0100

    updata_email screen responsive

--- lib/screens/profile/upadate_email.dart
+++ lib/screens/profile/upadate_email.dart
@@ -1,6 +1,8 @@
-import 'package:bolt_ui/constants/app_color.dart';
 import 'package:bolt_ui/widgets/custom_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../constants/app_color.dart';
 
 class UpdateEmail extends StatefulWidget {
   const UpdateEmail({super.key});
@@ -24,46 +26,57 @@ class _UpdateInfoState extends State<UpdateEmail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const BackButton(),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 12,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const BackButton(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 8.sp,
+                horizontal: 12.sp,
+              ),
+              child: Text(
+                "Update your email",
+                style: AppTextStyle.title1,
+              ),
             ),
-            child: Text(
-              "Update your email",
-              style: AppTextStyle.title1,
+            Padding(
+              padding: EdgeInsets.all(10.sp),
+              child: SizedBox(
+                height: 50.sp,
+                child: CustomFormField(
+                  labelText: "First name",
+                  controller: _emailController,
+                  focusNode: _emailFocusNode,
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: CustomFormField(
-              labelText: "First name",
-              controller: _emailController,
-              focusNode: _emailFocusNode,
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.sp,
+              ),
+              child: Text(
+                "We sent an email to verify this address"
+                "Please check your inbox.",
+                style: AppTextStyle.body1,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 12,
-            ),
-            child: Text(
-              "We sent an email to verify this address"
-              "Please check your inbox.",
-              style: AppTextStyle.body1,
-            ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Send again",
-              style: TextStyle(color: Colors.green),
-            ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.only(
+                  top: 10.sp), // Added some top padding for visual separation
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Send again",
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 16.sp), // Applied .sp to fontSize
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
